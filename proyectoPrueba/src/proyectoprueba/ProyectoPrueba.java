53b8470054dc4625e6f9b53c1c91f8176ec03cf6
AntonioDuranCarrillo <luis_ant238@hotmail.com>
2013-10-02 13:17:23 -0500

    acabo de hacer un contador del 1 al 5

---
+++ proyectoPrueba/src/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprueba;
+
+/**
+ *
+ * @author Omar
+ */
+public class ProyectoPrueba {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i < 5; i++) {
+            System.out.println("El numreo es: "+i);
+            
+        }
+    }
+}
